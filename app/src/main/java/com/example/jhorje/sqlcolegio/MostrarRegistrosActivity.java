90a28ed929d84e9a5f81672189a4ec802c5a7092
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-12-11 19:01:36 +0100

    Interfaz Estudiantes

--- app/src/main/java/com/example/jhorje/sqlcolegio/MostrarRegistrosActivity.java
+++ app/src/main/java/com/example/jhorje/sqlcolegio/MostrarRegistrosActivity.java
@@ -3,7 +3,14 @@ package com.example.jhorje.sqlcolegio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.jhorje.sqlcolegio.Adaptadores.AdaptadorEstudiantes;
 import com.example.jhorje.sqlcolegio.Objetos.Estudiante;
 import com.example.jhorje.sqlcolegio.Objetos.Profesor;
 
@@ -12,10 +19,36 @@ import java.util.ArrayList;
 public class MostrarRegistrosActivity extends AppCompatActivity {
 
     //TODO https://androidstudiofaqs.com/tutoriales/adapter-personalizado-en-android
-    
+    ArrayList<Estudiante> estudiantes;
+    ArrayList<Profesor> profesores;
+    AdaptadorEstudiantes adapter;
+    CheckBox boxEstudiantes, boxProfesores;
+    EditText editCiclo, editCurso;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_registros);
+
+        //Controlador Vista
+        boxEstudiantes = (CheckBox) findViewById(R.id.boxEstudiantes);
+        boxProfesores = (CheckBox) findViewById(R.id.boxProfesores);
+        editCiclo = (EditText) findViewById(R.id.editFiltroCiclo);
+        editCurso = (EditText) findViewById(R.id.editFiltroCurso);
+
+        estudiantes = new ArrayList<Estudiante>();
+
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+        estudiantes.add(new Estudiante("Jorge",19,"Florida","DAM",9));
+
+        ListView lv = (ListView) findViewById(R.id.listEstudiantes);
+        adapter = new AdaptadorEstudiantes(this, estudiantes);
+        lv.setAdapter(adapter);
     }
+
 }
